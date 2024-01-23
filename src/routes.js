793bd92df724727f414a76cef3f7c998617fb4a3
Brunofell <brunofelimarti@gmail.com>
2024-01-23 19:04:29 -0300

    ainda não terminei a sessão 2, faço amanhã

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
+import Favoritos from "pages/Favoritos";
 import Inicio from "./pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
+import Container from "components/Container";
 
 function AppRoutes(){
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio/>}></Route>
-            </Routes>
+            <Cabecalho></Cabecalho>
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </Container>
+            <Rodape></Rodape>
         </BrowserRouter>
     )
 }
